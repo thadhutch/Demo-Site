eda74c1b01453499b9a3f504c88d0067dc2ebceb
jonesliddy77 <jonesliddy77@gmail.com>
2021-09-07 19:35:27 -0500

    idk

--- demo-site/src/screens/About/index.js
+++ demo-site/src/screens/About/index.js
@@ -29,35 +29,35 @@ const About = () => {
 
     console.log(document.getElementById('spin-container'));
 
-    // obox = document.getElementById('drag-container');
-    // ospin = document.getElementById('spin-container');
-    // aImg = document.getElementById('img');
-    // aVid = document.getElementById('video');
-    // aEle = [aImg, aVid];
+    obox = document.getElementById('drag-container');
+    ospin = document.getElementById('spin-container');
+    aImg = document.getElementsByTagName('img');
+    aVid = document.getElementsByTagName('video');
+    aEle = [aImg, aVid];
   
-    // ospin.style.width = imgWidth + "px";
-    // ospin.style.height = imgHeight + "px";
+    ospin.style.width = imgWidth + "px";
+    ospin.style.height = imgHeight + "px";
   
   
-    // ground = document.getElementById('ground');
-    // ground.styles.width = radius * 3 + "px";
-    // ground.styles.height = radius * 3 + "px";
-    // init(100);
+    ground = document.getElementById('ground');
+    ground.styles.width = radius * 3 + "px";
+    ground.styles.height = radius * 3 + "px";
+    init(100);
 
 
-    // if (autoRotate) {
-    //   let animationName = (rotateSpeed > 0 ? 'spin' : 'spinRevert');
-    //   ospin.styles.animation = `${animationName} ${Math.abs(rotateSpeed)}s infinite linear`;
-    // }
+    if (autoRotate) {
+      let animationName = (rotateSpeed > 0 ? 'spin' : 'spinRevert');
+      ospin.styles.animation = `${animationName} ${Math.abs(rotateSpeed)}s infinite linear`;
+    }
   
   
-    // if (bgMusicURL) {
-    //   document.getElementById('music-container').innerHTML += `
-    // <audio src="${bgMusicURL}" ${bgMusicControls? 'controls': ''} autoplay loop>    
-    // <p>If you are reading this, it is because your browser does not support the audio element.</p>
-    // </audio>
-    // `;
-    // }
+    if (bgMusicURL) {
+      document.getElementById('music-container').innerHTML += `
+    <audio src="${bgMusicURL}" ${bgMusicControls? 'controls': ''} autoplay loop>    
+    <p>If you are reading this, it is because your browser does not support the audio element.</p>
+    </audio>
+    `;
+    }
   
   }, []);
 
@@ -134,18 +134,18 @@ const About = () => {
   return (
     <div className={styles.aboutPageContainer}>
       <div className={styles.aboutPage}>
-        <div id={styles.dragContainer}>
+        <div id="drag-container">
           <div id="spin-container">
-            <img id={styles.img} src="https://images.pexels.com/photos/206395/pexels-photo-206395.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="" />
-            <img id={styles.img} src="https://images.pexels.com/photos/1391498/pexels-photo-1391498.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="" />
-            <img id={styles.img} src="https://images.pexels.com/photos/1382731/pexels-photo-1382731.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="" />
-            <img id={styles.img} src="https://images.pexels.com/photos/1758144/pexels-photo-1758144.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="" />
-            <img id={styles.img} src="https://images.pexels.com/photos/1382734/pexels-photo-1382734.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="" />
-            <img id={styles.img} src="https://images.pexels.com/photos/1462636/pexels-photo-1462636.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="" />
+            <img src="https://images.pexels.com/photos/206395/pexels-photo-206395.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="" />
+            <img src="https://images.pexels.com/photos/1391498/pexels-photo-1391498.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="" />
+            <img src="https://images.pexels.com/photos/1382731/pexels-photo-1382731.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="" />
+            <img src="https://images.pexels.com/photos/1758144/pexels-photo-1758144.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="" />
+            <img src="https://images.pexels.com/photos/1382734/pexels-photo-1382734.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="" />
+            <img src="https://images.pexels.com/photos/1462636/pexels-photo-1462636.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="" />
             <a target="_blank" href="https://images.pexels.com/photos/139829/pexels-photo-139829.jpeg">
               <img src="https://images.pexels.com/photos/139829/pexels-photo-139829.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="" />
             </a>
-            <video id={styles.video} controls autoplay="autoplay" loop>
+            <video controls autoplay="autoplay" loop>
               <source src="https://player.vimeo.com/external/322244668.sd.mp4?s=338c48ac2dfcb1d4c0689968b5baf94eee6ca0c1&profile_id=165&oauth2_token_id=57447761" type="video/mp4" />
             </video>
             <p>3D Tiktok Carousel</p>
