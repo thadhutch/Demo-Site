85c200207fbf767fb5236e8ac39486cf0a26c1ec
Thad Hutcheson <thadhutch@gmail.com>
2021-09-14 02:18:40 -0500

    marketplace slider functionality and header gap

--- demo-site/src/screens/Marketplace/MarketplaceCardSliders/index.js
+++ demo-site/src/screens/Marketplace/MarketplaceCardSliders/index.js
@@ -16,8 +16,8 @@ const SlickArrow = ({ currentSlide, slideCount, children, ...props }) => (
   <button {...props}>{children}</button>
 );
 
-const MarketPlaceCardSlider = ({ classSection }) => {
-  const sliderSettings = {
+const MarketPlaceCardSlider = ({ currentSlide, slideCount, children }) => {
+  const settings = {
     infinite: false,
     speed: 500,
     slidesToShow: 4,
@@ -35,21 +35,26 @@ const MarketPlaceCardSlider = ({ classSection }) => {
     responsive: [
       {
         breakpoint: 1179,
-        sliderSettings: {
+        settings: {
           slidesToShow: 3,
         },
       },
       {
         breakpoint: 1023,
-        sliderSettings: {
+        settings: {
           slidesToShow: 2,
         },
       },
       {
         breakpoint: 767,
-        sliderSettings: {
+        settings: {
           slidesToShow: 2,
-          infinite: true,
+        },
+      },
+      {
+        breakpoint: 420,
+        settings: {
+        slidesToShow: 1,
         },
       },
     ],
@@ -62,7 +67,7 @@ const MarketPlaceCardSlider = ({ classSection }) => {
         <div className={styles.SliderWrapper1}>
         <h3 className={styles.cardSliderTitle}> Live Auctions</h3>
           <div className={styles.inner}>
-            <Slider className="bid-slider" {...sliderSettings}>
+            <Slider className="bid-slider" {...settings}>
               {bids.map((x, index) => (
                <Card key={index} className={styles.card} item={x}/>
               ))}
@@ -76,7 +81,7 @@ const MarketPlaceCardSlider = ({ classSection }) => {
         <div className={styles.SliderWrapper}>
         <h3 className={styles.cardSliderTitle}>Categories</h3>
           <div className={styles.inner}>
-            <Slider className="bid-slider" {...sliderSettings}>
+            <Slider className="bid-slider" {...settings}>
               {worlds.map((x, index) => (
                 <WorldsCards key={index} className={styles.card} item={x} />
               ))}
@@ -90,7 +95,7 @@ const MarketPlaceCardSlider = ({ classSection }) => {
         <div className={styles.SliderWrapper}>
           <h3 className={styles.cardSliderTitle}>Artists</h3>
           <div className={styles.inner}>
-            <Slider className="bid-slider" {...sliderSettings}>
+            <Slider className="bid-slider" {...settings}>
               {artistCards.map((x, index) => (
                 <ArtistCard key={index} item={x} />
               ))}
