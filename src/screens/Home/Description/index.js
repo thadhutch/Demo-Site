05ad9d9d50305af528e1e55bfa483e6d274849a6
jonesliddy77 <jonesliddy77@gmail.com>
2021-09-25 10:38:11 -0500

    touch ups

--- src/screens/Home/Description/index.js
+++ src/screens/Home/Description/index.js
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import cn from "classnames";
 import { Link } from "react-router-dom";
+import "./mouse.css"
 import styles from "./Description.module.sass";
 import VerifiedArtistButton from "./VerifiedArtistButton";
 import HomeHeroCards from "./HomeHeroCards";
@@ -12,30 +13,49 @@ const Description = () => {
       <div className={styles.homeHero}>
         <div className={styles.homeHeroLeft}>
           <div className={styles.homeHeroWords}>
-              <h1 className={styles.title}>Govern, Create, and Collect Digital Art</h1>
-              <h2>
+            <h1 className={styles.title}>Govern, Create, and Collect Digital Art</h1>
+            <h2>
               On a one of a kind feeless, ecological & defi marketplace.
-              </h2>
+            </h2>
           </div>
-            <div className={styles.btns}>
-              <Link className={cn("button", styles.button)} to="/marketplace">
-                Discover More
-              </Link>
-              <Link className={cn("button-stroke", styles.button)} to="/">
-                Earn SP
-              </Link>
-            </div>
+          <div className={styles.btns}>
+            <Link className={cn("button", styles.button)} to="/marketplace">
+              Discover More
+            </Link>
+            <Link className={cn("button-stroke", styles.button)} to="/">
+              Earn SP
+            </Link>
           </div>
-          <div className={styles.homeHeroFeature}>
-            <h2 className={styles.featuretitle}>Artist Spotlight</h2>
-            <div className={styles.FeaturedContent}>
-              <HomeHeroCards />
-            </div>
-            <div className={styles.artistInfo}>
+        </div>
+        <div className={styles.homeHeroFeature}>
+          <h2 className={styles.featuretitle}>Artist Spotlight</h2>
+          <div className={styles.FeaturedContent}>
+            <HomeHeroCards />
+          </div>
+          {/* <div className={styles.artistInfo}>
               hello
+            </div> */}
+        </div>
+      </div>
+      <div className={styles.moveContainer}>
+        {/* mouse scroll animation */}
+        {/* $(window).scroll(function () {
+            if ($(this).scrollTop() > 0) {
+              // can be whatever, 0 refers to the top space you allow
+              $('.move').slideUp('slow'); // Hide your element
+              } else {
+                $('.move').slideDown('slow'); 
+              // It's justmouse if you want to show back the element if we're back on top
+          }
+        }); */}
+        <div className="move">
+          <div className="mouse">
+            <div className="mouse-icon">
+              <span className="mouse-wheel"></span>
             </div>
           </div>
         </div>
+      </div>
     </div>
   );
 };
